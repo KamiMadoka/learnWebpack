86b2b299d406a241d21b6d948363200479afc4bf
Madoka <KamiMadoka@outlook.com>
2020-08-05 20:05:23 +0800

    管理资源

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 // + import _ from 'lodash';
 const _ = require('lodash');
+import './style.css';
+import Icon from './icon.svg';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
   
+    const myIcon = new Image()
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data)
     return element;
   }
   
