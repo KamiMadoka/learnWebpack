ce6693a41134bb477a136d0573962953d3b0df5e
Madoka <KamiMadoka@outlook.com>
2020-08-05 20:27:56 +0800

    管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    apps: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+      new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'HtmlWebpackPlugin'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
 };
