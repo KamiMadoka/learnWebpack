1443593c64353e805c9c34299e4f68ffcf8616f6
Madoka <KamiMadoka@outlook.com>
2020-08-05 20:07:41 +0800

    todo 管理输出

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 // + import _ from 'lodash';
 const _ = require('lodash');
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -10,12 +7,7 @@ function component() {
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-  
-    const myIcon = new Image()
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
 
-    console.log(Data)
     return element;
   }
   
